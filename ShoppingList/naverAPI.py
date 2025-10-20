f366033f5245a2bb84571a20381a8fb3595aab4d
tomung-puppy <yw3835@gmail.com>
2025-10-20 10:40:25 +0900

    add a function: generateCatId() - first category list

--- ShoppingList/naverAPI.py
+++ ShoppingList/naverAPI.py
@@ -5,8 +5,42 @@ import urllib.request
 import json
 from dotenv import load_dotenv
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from pathlib import Path
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+
 def generateCatId():
-    pass
+
+
+    driver_path = Path('ShoppingList/driver/chromedriver')
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(service=service)
+    
+    datalab_url = 'https://datalab.naver.com/shoppingInsight/sCategory.naver'
+    driver.get(datalab_url)
+
+    # first and second catid
+    first_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]/span'))
+    )
+    first_element.click()
+    first_soup = BeautifulSoup(driver.page_source, 'html.parser')
+    first_data = first_soup.find_all('div',  'set_period category')[0].find_all('ul', 'select_list scroll_cst')[0].find_all('a', 'option')
+    first_cat = []
+    for i, ele in enumerate(first_data):
+        cat = {}
+        cat['id']= int(ele['data-cid'])
+        cat['p_id'] = 0
+        cat['name'] = ele.get_text()
+        print(cat['id'], cat['p_id'], cat['name'])
+
+
+    driver.quit()
 
 def fetchShoppingDataFromNaver(client_id, client_secret,startDate, endDate, timeUnit, categoryNameCodeList, device=None, ages=None, gender=None):
     
@@ -46,15 +80,23 @@ def fetchShoppingDataFromNaver(client_id, client_secret,startDate, endDate, time
         print("Error Code:" + rescode)
 
 if __name__ == '__main__':
-    #example
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    dotenv_path = os.path.join(current_dir, '..', '.env')
-    load_dotenv(dotenv_path=dotenv_path)
-    client_id = os.getenv("NAVER_CLIENT_ID")
-    client_secret = os.getenv("NAVER_CLIENT_SECRET")
-    test_category_list = [
-        {"name":"신선식품", "param": ["10000108"]}, 
-        {"name":"가공식품", "param": ["50000002"]},
-        {"name":"건강식품", "param": ["10000115"]}
-    ]
-    fetchShoppingDataFromNaver(client_id,client_secret,"2025-10-02", "2025-10-09", "date",test_category_list)
+
+    a = 1
+
+    if a ==  0:
+        #example
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        dotenv_path = os.path.join(current_dir, '..', '.env')
+        load_dotenv(dotenv_path=dotenv_path)
+        client_id = os.getenv("NAVER_CLIENT_ID")
+        client_secret = os.getenv("NAVER_CLIENT_SECRET")
+        test_category_list = [
+            {"name":"신선식품", "param": ["10000108"]}, 
+            {"name":"가공식품", "param": ["50000002"]},
+            {"name":"건강식품", "param": ["10000115"]}
+        ]
+        fetchShoppingDataFromNaver(client_id,client_secret,"2025-10-02", "2025-10-09", "date",test_category_list)
+    elif a ==1 :
+        generateCatId()
+    else:
+        pass
